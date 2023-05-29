894956f987fb64340af0c370dca8f4df5ba4d0f7
Muhammad Fahad <m.fahad2001104@gmail.com>
2023-05-30 00:11:01 +0500

    Create Password Program Using Python
    Here i have give explanation of steps and code

--- python_loop.py
+++ python_loop.py
@@ -1,9 +1,9 @@
-password=1542
+# How to Create a password Program in simple way using Python--- 
 
-input_password=int(input("Enter Your Password: "))
-
-while password!=1542:
+password=1542 #set a password or you can ask from user and store it in a variable.
+input_password=int(input("Enter Your Password: ")) 
+while password!=1542:   # i am using while loop because i don't know that how much time user will input incorrect password 
     print("Try Again")
-
 else:
   print("Welcome")
+
